e6b4d997c085a3f6234f42021fc1d4c3419c6eb8
xiaofeng <sxf1052566766@163.com>
2018-11-09 12:41:56 +0800

    flask

--- FLASK/app.py
+++ FLASK/app.py
@@ -1,33 +1,82 @@
-'''
-File: app.py
-Project: FLASK
-File Created: Saturday, 6th October 2018 10:29:58 pm
-Author: icey (sxf1052566766@163.com)
------
-Last Modified: Saturday, 6th October 2018 10:30:43 pm
-Modified By: icey (sxf1052566766@163.com>)
------
-Copyright 2018.06 - 2018 onion Math, onion Math
-'''
 
-from flask import Flask, render_template
-import config
+# encoding:utf-8
+#!/usr/bin/env python
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, jsonify, request, make_response, send_from_directory, abort
+import time
+import os
+import base64
+import datetime
+import random
 
 app = Flask(__name__)
-app.config.from_object(config)
+UPLOAD_FOLDER = 'upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+basedir = os.path.abspath(os.path.dirname(__file__))
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF'])
+
+
+class Pic_str:
+    def create_uuid(self):  # 生成唯一的图片的名称字符串，防止图片显示时的重名问题
+        nowTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")  # 生成当前时间
+        randomNum = random.randint(0, 100)  # 生成的随机整数n，其中0<=n<=100
+        if randomNum <= 10:
+            randomNum = str(0) + str(randomNum)
+        uniqueNum = str(nowTime) + str(randomNum)
+        return uniqueNum
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
-def hello_world():
-    
+def upload_test():
     return render_template('index.html')
 
 
-# @app.route('/article/<id>')
-# def article(id):
-#     log = '请求的参数为：{}'.format(id)
-#     return log
+# 上传文件
+@app.route('/', methods=['POST'], strict_slashes=False)
+def api_upload():
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    f = request.files['photo']
+    if f and allowed_file(f.filename):
+        fname = secure_filename(f.filename)
+        print(fname)
+        ext = fname.rsplit('.', 1)[1]
+        new_filename = Pic_str().create_uuid() + '.' + ext
+        f.save(os.path.join(file_dir, new_filename))
+
+        return jsonify({"success": 0, "msg": "上传成功"})
+    else:
+        return jsonify({"error": 1001, "msg": "上传失败"})
+
+
+@app.route('/download/<string:filename>', methods=['GET'])
+def download(filename):
+    if request.method == "GET":
+        if os.path.isfile(os.path.join('upload', filename)):
+            return send_from_directory('upload', filename, as_attachment=True)
+        pass
+
+
+# show photo
+@app.route('/show/<string:filename>', methods=['GET'])
+def show_photo(filename):
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+    if request.method == 'GET':
+        if filename is None:
+            pass
+        else:
+            image_data = open(os.path.join(file_dir, '%s' % filename), "rb").read()
+            response = make_response(image_data)
+            response.headers['Content-Type'] = 'image/png'
+            return response
+    else:
+        pass
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0',port=1988,debug=True)
--- FLASK/readme.md
+++ FLASK/readme.md
@@ -1,3 +1,32 @@
 # 配置文件
 - 使用'config.py'文件
 - 使用'app.config.from_object(config)'加载配置参数
+
+
+## 1.数据库安装
+- Mac 本地启动mysql
+
+  ```bash
+  # 启动
+  mysql -uroot -p 
+  # 输入密码
+  ```
+
+- 安装mysql-python
+
+  ```bash
+  sudo pip install mysql-python
+  ```
+
+- 安装flask-sqlalchemy
+
+  使用flask与数据库之间进行关系映射，对数据库进行增删改查等操作。可以想在Python中操作类一样对数据库进行操作，数据库中的表抽象成了类，数据库中的每条数据对应类中的一个对象。
+
+  ```bash
+  # pip 安装
+  pip install flask-sqlalchemy
+  # conda 安装
+  conda install -c conda-forge flask-sqlalchemy 
+  ```
+
+- sqlalchemy连接数据库
